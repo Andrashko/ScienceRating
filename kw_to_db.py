316778b1e4d26ad542b589c6486b51958b664166
Andrashko <yurii.andrashko@uzhnu.edu.ua>
2021-12-26 15:41:13 +0200

    migrate

--- kw_to_db.py
+++ kw_to_db.py
@@ -6,29 +6,20 @@ import json
 
 db_session.global_init("db/database.db")
 db_sess = db_session.create_session()
+from data.database.keywords import Keywords
+from data.database.ukraine_scientists import Ukraine_Scientists
+from data.database.keywords import Keywords
+from data.Standart import db_session
+import json
+
+db_session.global_init("db/database.db")
+db_sess = db_session.create_session()
 with open('db/scopus.json', encoding='utf-8') as file:
     scop = json.load(file)
-    keywords_frequency = {"science":1}
     for scientist in db_sess.query(Ukraine_Scientists).all():
-        for i in scop:
-            if i['name'] == scientist.name:
-                keywords = i.get("keywords")
-                if keywords:
-                    count = 20
-                    skip = False
-                    for kw in keywords:
-                        for w in scientist.keywords:
-                            if w.word == kw:
-                                skip = True
-                                break
-                        if skip:
-                            continue
-                        dbkw = Keywords(word=kw, priority=count, scientist_id=scientist.id)
-                        db_sess.add(dbkw)
-                        db_sess.commit()
-
-                        if count <= 10:
-                            count -=1
-                        else:
-                            count -=5
-                print(scientist.id)
+        if scientist.science:
+            dbkw = Keywords(word=scientist.science, priority=30, scientist_id=scientist.id)
+            db_sess.add(dbkw)
+            db_sess.commit()  
+        if scientist.id % 100 == 0:
+            print (scientist.id) 
